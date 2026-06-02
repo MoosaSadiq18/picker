7c1617b6c0d5bb4ec4ac06857590b7638576c10e
MoosaSadiq18 <ahmadmoosasadiq@gmail.com>
2026-06-02 12:52:11 +0500

    python s3 config check

---
+++ src/main/java/org/example/picker/facial_recognition/python/pythonS3.py
@@ -0,0 +1,7 @@
+import boto3
+
+session = boto3.Session(
+    aws_secret_access_key = 'OL28qZxyEobnNHoBkTgkYaOh/rOnThZ59tg4Stex',
+    aws_access_key_id = 'AKIASHFYMNXR53D2DB4V',
+    region_name = 'eu-north-1'
+)
